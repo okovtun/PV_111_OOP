8f497603e99921fa7a3360a29aab154fed0da50d
Clayman <Clayman@DESKTOP-DM8E77U>
2022-06-03 21:00:33 +0300

    Constructors header initialisation DONE

--- IntroductionToOOP/ClassString/main.cpp
+++ IntroductionToOOP/ClassString/main.cpp
@@ -23,25 +23,23 @@ public:
 
 
 	//				Constructors:
-	explicit String(int size = 80)
+	explicit String(int size = 80) :size(size), str(new char[size] {})
 	{
-		this->size = size;
-		this->str = new char[size] {};
+		//this->size = size;
+		//this->str = new char[size] {};
 		cout << "Def1aConstruct:\t" << this << endl;
 	}
-	String(const char* str)
+	String(const char* str) :size(strlen(str) + 1), str(new char[size] {})
 	{
-		this->size = strlen(str) + 1;
-		this->str = new char[size] {};
+		//this->size = strlen(str) + 1;
+		//this->str = new char[size] {};
 		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "Constructor:\t" << this << endl;
 	}
-	String(const String& other)
+	String(const String& other) :size(other.size), str(new char[size] {})
 	{
-		this->size = other.size;
 		//CopyConstrutor должен выполнять DeepCopy (Побитовое копирование),
 		//т.е., выделить новую память, и скопировать в нее содержимое другого объекта
-		this->str = new char[size] {};
 		for (int i = 0; i < size; i++)
 		{
 			this->str[i] = other.str[i];
@@ -49,12 +47,10 @@ public:
 		//---------------------------------------------------------------------------
 		cout << "CopyConstructor:" << this << endl;
 	}
-	String(String&& other)
+	String(String&& other) :size(other.size), str(other.str)
 	{
 		//MoveConstructor выполнет ShellowCopy (Повершностное копирование)
-		this->size = other.size;
-		this->str = other.str;	//Копируем адрес памяти, пренадлежащей другому объекту
-		//other.size = 0;
+		other.size = 0;
 		other.str = nullptr;	//Зануляем адрес памяти в другом объекте, чтобы эту память НЕ удалил деструктор
 		cout << "MoveConstructor:" << this << endl;
 	}
@@ -126,7 +122,8 @@ std::ostream& operator<<(std::ostream& os, const String& obj)
 }
 
 //#define CONSTRUCTORS_CHECK
-//#define MOVE_METHODS_CHECK
+#define MOVE_METHODS_CHECK
+//#define CALLING_CONSTRUCTORS
 
 void main()
 {
@@ -162,7 +159,8 @@ void main()
 	//str3.print();
 	cout << str3 << endl;
 #endif // MOVE_METHODS_CHECK
-
+	//StringHeaderInitialising
+#ifdef CALLING_CONSTRUCTORS
 	String str1;			//Default constructor
 	str1.print();
 	String str2(55);		//Single-argument constructor (int)
@@ -178,6 +176,8 @@ void main()
 					  и возвращает объек класса String
 	String str7{};	//Явный вызов конструктора по умолчанию
 	str7.print();
+#endif // CALLING_CONSTRUCTORS
+
 }
 
 /*
