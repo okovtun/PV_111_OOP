2135d62ef87e05b258bf40d91962d6c613219fbf
Clayman <Clayman@DESKTOP-DM8E77U>
2022-06-03 22:07:39 +0300

    Methods out of class

--- IntroductionToOOP/ClassString/main.cpp
+++ IntroductionToOOP/ClassString/main.cpp
@@ -4,97 +4,132 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+//////////////////////////////////////////////////////////////////////////////
+//////////			CLASS DECLARATION - ОБЪЯВЛЕНИЕ КЛАССА			//////////
+//////////////////////////////////////////////////////////////////////////////
+
 class String
 {
 	int size;	//Размерстроки в Байтах
 	char* str;	//Указатель на строку
 public:
-	int get_size()const
-	{
-		return size;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
 
 
 	//				Constructors:
-	explicit String(int size = 80) :size(size), str(new char[size] {})
-	{
-		//this->size = size;
-		//this->str = new char[size] {};
-		cout << "Def1aConstruct:\t" << this << endl;
-	}
-	String(const char* str) :String(strlen(str) + 1)
-	{
-		for (int i = 0; i < size; i++)this->str[i] = str[i];
-		cout << "Constructor:\t" << this << endl;
-	}
-	String(const String& other) :String(other.str)
-	{
-		//CopyConstrutor должен выполнять DeepCopy (Побитовое копирование),
-		//т.е., выделить новую память, и скопировать в нее содержимое другого объекта
-		cout << "CopyConstructor:" << this << endl;
-	}
-	String(String&& other) :size(other.size), str(other.str)
-	{
-		//MoveConstructor выполнет ShellowCopy (Повершностное копирование)
-		other.size = 0;
-		other.str = nullptr;	//Зануляем адрес памяти в другом объекте, чтобы эту память НЕ удалил деструктор
-		cout << "MoveConstructor:" << this << endl;
-	}
-	~String()
-	{
-		delete[] this->str;
-		cout << "Destructor:\t" << this << endl;
-	}
+	explicit String(int size = 80);
+	String(const char* str);
+	String(const String& other);
+	String(String&& other);
+	~String();
 
 	//				Operators:
-	String& operator=(const String& other)
-	{
-		/*int a = 2;
-		int b = 3;
-		a = b;*/
-		if (this == &other)return *this;
-		delete[] this->str;
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyAssignment:\t" << this << endl;
-		return *this;
-	}
-	String& operator=(String&& other)
-	{
-		delete[] this->str;
-		this->size = other.size;
-		this->str = other.str;
-		other.size = 0;
-		other.str = nullptr;
-		cout << "MoveAssignment:\t" << this << endl;
-		return *this;
-	}
-	const char& operator[](int i)const
-	{
-		return str[i];
-	}
-	char& operator[](int i)
-	{
-		return str[i];
-	}
-
+	String& operator=(const String& other);
+	String& operator=(String&& other);
+	const char& operator[](int i)const;
+	char& operator[](int i);
 
 	//				Methods:
-	void print()const
-	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
-	}
+	void print()const;
 };
+
+//////////////////////////////////////////////////////////////////////////////
+//////////		CLASS DECLARATION END - КОНЕЦ ОБЪЯВЛЕНИЯ КЛАССА		//////////
+//////////////////////////////////////////////////////////////////////////////
+
+//--------------------------------------------------------------------------//
+
+//////////////////////////////////////////////////////////////////////////////
+//////////			CLASS DEFINITION - ОПРЕДЕЛЕНИЕ КЛАССА			//////////
+//////////////////////////////////////////////////////////////////////////////
+
+int String::get_size()const
+{
+	return size;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+
+
+//				Constructors:
+String::String(int size) :size(size), str(new char[size] {})
+{
+	//this->size = size;
+	//this->str = new char[size] {};
+	cout << "Def1aConstruct:\t" << this << endl;
+}
+String::String(const char* str) :String(strlen(str) + 1)
+{
+	for (int i = 0; i < size; i++)this->str[i] = str[i];
+	cout << "Constructor:\t" << this << endl;
+}
+String::String(const String& other) :String(other.str)
+{
+	//CopyConstrutor должен выполнять DeepCopy (Побитовое копирование),
+	//т.е., выделить новую память, и скопировать в нее содержимое другого объекта
+	cout << "CopyConstructor:" << this << endl;
+}
+String::String(String&& other) :size(other.size), str(other.str)
+{
+	//MoveConstructor выполнет ShellowCopy (Повершностное копирование)
+	other.size = 0;
+	other.str = nullptr;	//Зануляем адрес памяти в другом объекте, чтобы эту память НЕ удалил деструктор
+	cout << "MoveConstructor:" << this << endl;
+}
+String::~String()
+{
+	delete[] this->str;
+	cout << "Destructor:\t" << this << endl;
+}
+
+//				Operators:
+String& String::operator=(const String& other)
+{
+	/*int a = 2;
+	int b = 3;
+	a = b;*/
+	if (this == &other)return *this;
+	delete[] this->str;
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyAssignment:\t" << this << endl;
+	return *this;
+}
+//Global scope - Глобальное пространство имен
+String& String::operator=(String&& other)
+{
+	delete[] this->str;
+	this->size = other.size;
+	this->str = other.str;
+	other.size = 0;
+	other.str = nullptr;
+	cout << "MoveAssignment:\t" << this << endl;
+	return *this;
+}
+const char& String::operator[](int i)const
+{
+	return str[i];
+}
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+//				Methods:
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
 
 String operator+(const String& left, const String& right)
 {
@@ -114,6 +149,10 @@ std::ostream& operator<<(std::ostream& os, const String& obj)
 {
 	return os << obj.get_str();
 }
+
+//////////////////////////////////////////////////////////////////////////////
+//////////		CLASS DEFINITION END - КОНЕЦ ОПРЕДЕЛЕНИЯ КЛАССА		//////////
+//////////////////////////////////////////////////////////////////////////////
 
 //#define CONSTRUCTORS_CHECK
 #define MOVE_METHODS_CHECK
