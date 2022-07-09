309f5d1b5ee2d7295b1f155deb3c1ba17f4e3e71
Clayman <Clayman@DESKTOP-DM8E77U>
2022-07-09 18:51:41 +0300

    CopyConstructor in ForwardList

--- DataContainers/ForwardList/main.cpp
+++ DataContainers/ForwardList/main.cpp
@@ -29,15 +29,42 @@ int Element::count = 0;
 class ForwardList
 {
 	Element* Head;	//Голова списка
+	unsigned int size;
 public:
 	ForwardList()
 	{
 		Head = nullptr; //Когда Голова списка указывает на 0 - список пуст
+		size = 0;
 		cout << "LConstructor:\t" << this << endl;
+	}
+	ForwardList(const ForwardList& other):ForwardList()
+	{
+		Element* Temp = other.Head;
+		while (Temp)
+		{
+			push_back(Temp->Data);
+			Temp = Temp->pNext;
+		}
+		cout << "CopyConstructor:\t" << this << endl;
 	}
 	~ForwardList()
 	{
+		while (Head)pop_front();
 		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//				Operators:
+	ForwardList& operator=(const ForwardList& other)
+	{
+		while (Head)pop_front();
+		Element* Temp = other.Head;
+		while (Temp)
+		{
+			push_back(Temp->Data);
+			Temp = Temp->pNext;
+		}
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
 	}
 
 	//				Adding Elements:
@@ -49,6 +76,7 @@ public:
 		New->pNext = Head;
 		//3) Говорим, что новый элемент является головой списка:
 		Head = New;
+		size++;
 	}
 	void push_back(int Data)
 	{
@@ -60,15 +88,23 @@ public:
 		while (Temp->pNext)Temp = Temp->pNext;
 		//3) Присоединяем новый элемент к списку:
 		Temp->pNext = New;
+		size++;
 	}
 	void insert(int Data, int Index)
 	{
 		if (Index == 0)return push_front(Data);
+		if (Index == size)return push_back(Data);
+		if (Index > size)
+		{
+			cout << "Error: Out of range" << endl;
+			return;
+		}
 		Element* New = new Element(Data);
 		Element* Temp = Head;
 		for (int i = 0; i < Index - 1; i++)Temp = Temp->pNext;
 		New->pNext = Temp->pNext;
 		Temp->pNext = New;
+		size++;
 	}
 	//				Removing elements:
 	void pop_front()
@@ -79,6 +115,7 @@ public:
 		Head = Head->pNext;
 		//3) Удаляем элемент из памяти
 		delete erased;
+		size--;
 	}
 	void pop_back()
 	{
@@ -86,6 +123,7 @@ public:
 		while (Temp->pNext->pNext)Temp = Temp->pNext;
 		delete Temp->pNext;
 		Temp->pNext = nullptr;
+		size--;
 	}
 
 	//				Methods:
@@ -99,12 +137,18 @@ public:
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext;	//Переход на следующий элемент
 		}
+		cout << "Количество элементов списка:" << size << endl;
+		cout << "Общее количество элементов: " << Element::count << endl;
 	}
 };
+
+#define BASE_CHECK
+//#define COUNT_CHECK
 
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef BASE_CHECK
 	int n;
 	cout << "Введите размер списка: "; cin >> n;
 	int arr[5];
@@ -115,13 +159,35 @@ void main()
 		list.push_back(rand() % 100);
 	}
 	list.print();
-	list.pop_front();
-	//list.pop_back();
+	/*list.pop_front();
 	list.print();
 	int value;
 	int index;
 	cout << "Введите значение добавляемого элемента: "; cin >> value;
 	cout << "Введите индекс добавляемого элемента: "; cin >> index;
 	list.insert(value, index);
-	list.print();
+	list.print();*/
+
+	//ForwardList list2 = list;	//CopyConstructor
+	ForwardList list2;
+	list2 = list;
+	list2.print();
+#endif // BASE_CHECK
+
+#ifdef COUNT_CHECK
+	ForwardList list1;
+	list1.push_back(3);
+	list1.push_back(5);
+	list1.push_back(8);
+	list1.push_back(13);
+	list1.push_back(21);
+	list1.print();
+
+	ForwardList list2;
+	list2.push_back(34);
+	list2.push_back(55);
+	list2.push_back(89);
+	list2.print();
+#endif // COUNT_CHECK
+
 }
