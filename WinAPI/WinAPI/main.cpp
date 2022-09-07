06cc028a4425288b31fec597cd479f0aea09c4fd
Clayman <Clayman@DESKTOP-DM8E77U>
2022-09-07 20:24:30 +0300

    IDD_DIALOG1

---
+++ WinAPI/WinAPI/main.cpp
@@ -0,0 +1,33 @@
+﻿//WinAPI
+#include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	//MessageBox(NULL, "Привет! Это окно сообщения", "Заголовок окна", MB_YESNO | MB_ICONINFORMATION | MB_HELP | MB_DEFBUTTON2 | MB_SYSTEMMODAL);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:	//Создаем элементы окна
+		break;
+	case WM_COMMAND:	//Обрабатываем сообщения нажатия на кнопки...
+		switch (LOWORD(wParam))
+		{
+		case IDOK: MessageBox(NULL, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDCANCEL: 
+			EndDialog(hwnd, 0);
+			return FALSE;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
